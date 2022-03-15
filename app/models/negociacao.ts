b52f7099f2026150d81355e4fc56be3220b28d11
claudio.coimbra <claudiojccoimbra@gmail.com>
2022-03-15 12:24:18 -0300

    atualização e melhoria de sintaxe e métodos

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -9,4 +9,16 @@ export class Negociacao {
     get volume(): number {
         return this.quantidade * this.valor;
     }
+
+    public static CriaDe(dataString: string, quantidadeString: string, valorString: string ) : Negociacao{
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(
+           date,
+           quantidade,
+           valor
+        );
+    }
 }
