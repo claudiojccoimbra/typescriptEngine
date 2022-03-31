3f3d28a1300dd0178b88c94f460fe73e44f92ee9
claudio.coimbra <claudiojccoimbra@gmail.com>
2022-03-31 13:19:29 -0300

    adicionando decorator de propriedade

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -3,22 +3,17 @@ import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
     protected elemento: HTMLElement;
-    private escapar = false;
-
-    constructor(seletor : string, escapar?: boolean) {
+    
+    constructor(seletor : string) {
         const elemento = document.querySelector(seletor);
         if(elemento){
             this.elemento = elemento as HTMLElement;
         }else{
             throw Error('Seletor não existe no DOM')
         }
-        if(escapar){
-            this.escapar = escapar;
-        }
     }
 
-    @inspect
-    @logarTempoDeExecucao(true)
+
     public update(model: T): void{
         let template = this.template(model);
         this.elemento.innerHTML = template;
